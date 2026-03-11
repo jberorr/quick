db6f7d3b55b7389e3be2362ab992c34c6a5980a5
barmonjohnny <barmonjohnny@gmail.com>
2026-03-11 01:11:42 +0000

    new fix

--- application/app.php
+++ application/app.php
@@ -1,7 +1,7 @@
 <?php
 if(!defined('APP_RUN')) exit('No direct access allowed');
 $file_build = '4803';
-require 'vendor/autoload.php';
+// require 'vendor/autoload.php';
 // Include PHP compatibility helper for older function support
 require 'application/helpers/php_compatibility.php';
 require 'application/helpers/ibilling_bootstrap.php';
--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 define('APP_RUN', true);
 require ('application/app.php');
